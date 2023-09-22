d1fc24e2f8c3dd0280e251129863ef6b3e547da1
alexusernamebruh <85073684+alexusernamebruh@users.noreply.github.com>
2023-09-22 00:02:12 +0000

    start!

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,66 @@
-import { useCallback, useEffect, useState } from 'react'
-import Button from '../components/Button'
-import ClickCount from '../components/ClickCount'
-import styles from '../styles/home.module.css'
-
-function throwError() {
-  console.log(
-    // The function body() is not defined
-    document.body()
-  )
-}
+import { useCallback, useEffect, useState } from 'react';
+import Typewriter from 'typewriter-effect';
 
 function Home() {
-  const [count, setCount] = useState(0)
-  const increment = useCallback(() => {
-    setCount((v) => v + 1)
-  }, [setCount])
-
-  useEffect(() => {
-    const r = setInterval(() => {
-      increment()
-    }, 1000)
-
-    return () => {
-      clearInterval(r)
-    }
-  }, [increment])
-
   return (
-    <main className={styles.main}>
-      <h1>Fast Refresh Demo</h1>
-      <p>
-        Fast Refresh is a Next.js feature that gives you instantaneous feedback
-        on edits made to your React components, without ever losing component
-        state.
-      </p>
-      <hr className={styles.hr} />
-      <div>
-        <p>
-          Auto incrementing value. The counter won't reset after edits or if
-          there are errors.
-        </p>
-        <p>Current value: {count}</p>
+    <div>
+      <div className="p-8 w-screen flex flex-col h-screen bg-cover bg-no-repeat bg-[url('https://a-static.besthdwallpaper.com/vibrant-abstract-waves-wallpaper-3840x2160-16986_54.jpg')]">
+        <div className='flex mx-auto mt-7'>
+          <p className='text-7xl font-semibold tracking-wide text-gray-400'>
+            E-Portfolio
+          </p>
+          <p className='text-7xl font-semibold tracking-wide text-white absolute -ml-1.5'>
+            E-Portfolio
+          </p>
+        </div>
+        <div className='flex mx-auto mt-2'>
+          <p className='text-5xl mx-auto text-gray-400 font-semibold'>
+            By Alex Yang
+          </p>
+          <p className='text-5xl mx-auto text-white absolute -ml-1 font-semibold'>
+            By Alex Yang
+          </p>
+        </div>
+        <img
+          className='mx-auto rounded-full w-[10rem] h-[10rem] border-4 border-white shadow mt-4'
+          src='https://avatars.githubusercontent.com/u/85073684?v=4'
+        />
+        <div className='p-2 rounded-2xl bg-black bg-opacity-20 mx-auto'>
+          <p className='text-6xl font-semibold text-white mt-8 text-center mb-2'>
+            I am a...
+          </p>
+          <div className='flex flex-col mx-auto'>
+            <div className='flex text-center text-8xl font-semibold text-white'>
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString('Student')
+                    .pauseFor(2500)
+                    .deleteAll()
+                    .typeString('Coder')
+                    .pauseFor(2500)
+                    .deleteAll()
+                    .typeString('Musician')
+                    .pauseFor(2500)
+                    .deleteAll()
+                    .typeString('Friend')
+                    .pauseFor(2500)
+                    .deleteAll()
+                    .typeString('Engineer')
+                    .pauseFor(2500)
+                    .deleteAll()
+                    .start();
+                }}
+                options={{
+                  loop: true,
+                }}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <hr className={styles.hr} />
-      <div>
-        <p>Component with state.</p>
-        <ClickCount />
-      </div>
-      <hr className={styles.hr} />
-      <div>
-        <p>
-          The button below will throw 2 errors. You'll see the error overlay to
-          let you know about the errors but it won't break the page or reset
-          your state.
-        </p>
-        <Button
-          onClick={(e) => {
-            setTimeout(() => document.parentNode(), 0)
-            throwError()
-          }}
-        >
-          Throw an Error
-        </Button>
-      </div>
-      <hr className={styles.hr} />
-    </main>
-  )
+    </div>
+  );
 }
 
-export default Home
+export default Home;
